c8da5af4614d42094e0a596597db6c21aa3203b7
vamcr <vamzivijaysrikanth@gmail.com>
2020-04-23 16:01:02 +0530

    Update Ant.java

--- Ant.java
+++ Ant.java
@@ -1,6 +1,7 @@
-package demo;
-class Ant{
-public static void main(String args[]){
-System.out.println("Hello Gradle");
-}
+package oata;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
